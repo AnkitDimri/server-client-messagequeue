da6e80bcae1584f2c4c818a45f7fb0e0641549ed
Ankit <ankitdimri1999@gmail.com>
2019-11-05 23:44:06 +0530

    process counting added

--- client.sh
+++ client.sh
@@ -11,5 +11,12 @@ g++ client.cpp -o clnt
 echo "compiled ..."
 # run the client
 
+count=0
+ps_running=$(pidof client)
+for pids in $ps_running
+do
+  count=`expr $count + 1`
+done
+
 echo "starting client..."
 ./clnt
